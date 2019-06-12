62bcce99aac563279510c05bcc64a81afed8345a
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-06-12 10:46:42 -0300

    projeto-base criado, dependências essenciais adicionadas.

---
+++ backend-psoft/src/main/java/com/backend/psoft/ProjetoPsoftApplication.java
@@ -0,0 +1,13 @@
+package com.backend.psoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPsoftApplication.class, args);
+	}
+
+}
