d14de7129dcc8f5a61fcc4f93abf29ec3d619f9a
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2019-06-20 00:18:20 -0300

    Rebobinando...

--- src/main/java/com/backend/psoft/model/User.java
+++ src/main/java/com/backend/psoft/model/User.java
@@ -1,12 +1,7 @@
 package com.backend.psoft.model;
 
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -27,11 +22,6 @@ public class User {
 	@NotNull
 	@Size(min = 5, max = 15)
 	private String password;
-	
-	@Column
-	@ElementCollection(targetClass = User.class)
-	@ManyToMany(mappedBy = "likes")
-	private List<Profile> profiles;
 		
 	public User () {
 		
